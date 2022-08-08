9e98d1d9ace9e750022a5912f4aced95f847c639
Arkadiusz Machalica <a.machalica@selleo.com>
2022-08-08 11:05:15 +0200

    Embed sudoku.com

--- src/App.jsx
+++ src/App.jsx
@@ -27,33 +27,44 @@ function App() {
 
   return (
     <div className="App">
-      <form ref={formRef}>
-        <label htmlFor='number'>Number of digits</label>
-        <select ref={numberRef} onChange={handleChange} id='number' name='number' style={{width: '120px'}}>
-          <option value='2'>2</option>
-          <option value='3'>3</option>
-          <option value='4'>4</option>
-          <option value='5'>5</option>
-        </select>
-        <br/>
-        <label htmlFor='sum'>Sum of digits</label>
-        <select onChange={handleChange} id='sum' name='sum' required style={{width: '120px'}}>
-          {sumsOptions && Object.keys(sumsOptions).map((sum) => (
-            <option key={sum} value={sum}>{sum}</option>
-          ))}
-        </select>
-        <br/>
+      <div className="Form-wrapper">
+        <form ref={formRef}>
+          <label htmlFor='number'>Number of digits</label>
+          <select ref={numberRef} onChange={handleChange} id='number' name='number' style={{width: '120px'}}>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+          </select>
+          <br/>
+          <label htmlFor='sum'>Sum of digits</label>
+          <select onChange={handleChange} id='sum' name='sum' required style={{width: '120px'}}>
+            {sumsOptions && Object.keys(sumsOptions).map((sum) => (
+              <option key={sum} value={sum}>{sum}</option>
+            ))}
+          </select>
+          <br/>
 
-        <label htmlFor='included'>Required digits</label>
-        <input onChange={handleChange} id='included' name='included' style={{width: '120px'}}/>
-        <br/>
-        <label htmlFor='excluded'>Prevented digits</label>
-        <input onChange={handleChange} id='excluded' name='excluded' style={{width: '120px'}}/>
-      </form>
+          <label htmlFor='included'>Required digits</label>
+          <input onChange={handleChange} id='included' name='included' style={{width: '120px'}}/>
+          <br/>
+          <label htmlFor='excluded'>Prevented digits</label>
+          <input onChange={handleChange} id='excluded' name='excluded' style={{width: '120px'}}/>
+        </form>
 
-      {!!results?.length && <p>Result: <ul>{results.map((result) =>
-        <li><b>{result}</b></li>
-      )}</ul></p>}
+        {!!results?.length && <p>Result: <ul>{results.map((result) =>
+          <li><b>{result}</b></li>
+        )}</ul></p>}
+      </div>
+
+
+      <iframe
+        id="inlineFrameExample"
+        title="Inline Frame Example"
+        width="600"
+        height="100%"
+        src="https://sudoku.com/"
+      />
     </div>
   );
 }
