c17ab132522a67e07aa5a4a7b7b02f1c5b0dd2f0
Arkadiusz Machalica <a.machalica@selleo.com>
2022-08-10 11:25:58 +0200

    Improve styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import { sums } from "./logic/sums";
 function App() {
   const [data, setData] = useState({ number: '2', sum: '3', results: [] })
 
-  const handleChange = ({target: {name, value}}) => {
+  const handleChange = ({target}) => {
+    const {name, value} = target
+    console.log(`target: `, target);
     setData((prev) => {
       const newData = {...prev, [name]: value}
 
-      if (name === 'number') {
-        newData.sum = Object.keys(sums[parseInt(newData.number)] || {})[0] || null
+      console.log({ name, value });
+      if (name === 'number' && !sums[parseInt(newData.number)][parseInt(newData.sum)]) {
+        newData.sum = Object.keys(sums[parseInt(newData.number)])[0]
       }
 
       newData.results = check(newData)
@@ -24,41 +27,47 @@ function App() {
   return (
     <div className="App">
       <div className="Form-wrapper">
-        <form>
-          <fieldset onChange={handleChange} className="fieldset-number">
-            <legend>Number of digits:</legend>
+        <fieldset onChange={handleChange} className="fieldset-number">
+          <legend>Number of digits:</legend>
 
-            {['2','3','4','5'].map((digit) => (
-              <Fragment key={digit}>
-                <input type="radio" id={digit} name="number" value={digit} defaultChecked={digit === 2} style={{display: "none"}}/>
-                <label htmlFor={digit} className={`number-label ${data.number === digit ? '-selected' : ''}`}>{digit}</label>
-              </Fragment>
-            ))}
-          </fieldset>
-          <br/>
+          {['2','3','4','5'].map((digit) => (
+            <Fragment key={digit}>
+              <input type="radio" id={`number-${digit}`} name="number" value={digit} defaultChecked={digit === 2} style={{display: "none"}}/>
+              <label htmlFor={`number-${digit}`} className={`number-label ${data.number === digit ? '-selected' : ''}`}>{digit}</label>
+            </Fragment>
+          ))}
+        </fieldset>
 
-          <fieldset onChange={handleChange} className="fieldset-number -constant-height" key={data.number}>
-            <legend>Sum of digits:</legend>
+        <fieldset onChange={handleChange} className="fieldset-number -constant-height" key={data.number}>
+          <legend>Sum of digits:</legend>
 
-            {Object.keys(sumsOptions).map((digit) => (
-              <Fragment key={digit}>
-                <input type="radio" id={digit} name="sum" value={digit} style={{display: "none"}}/>
-                <label htmlFor={digit} className={`number-label ${data.sum === digit ? '-selected' : ''}`}>{digit}</label>
-              </Fragment>
-            ))}
-          </fieldset>
-          <br/>
+          {Object.keys(sumsOptions).map((digit) => (
+            <Fragment key={digit}>
+              <input type="radio" id={`sum-${digit}`} name="sum" value={digit} style={{display: "none"}}/>
+              <label htmlFor={`sum-${digit}`} className={`number-label ${data.sum === digit ? '-selected' : ''}`}>{digit}</label>
+            </Fragment>
+          ))}
+        </fieldset>
 
-          <label htmlFor='included'>Required digits</label>
-          <input onChange={handleChange} id='included' name='included' style={{width: '120px'}}/>
-          <br/>
-          <label htmlFor='excluded'>Prevented digits</label>
-          <input onChange={handleChange} id='excluded' name='excluded' style={{width: '120px'}}/>
-        </form>
+        <div>
+          <label className="cluded-label" htmlFor='included'>Required digits: </label>
+          <input onChange={handleChange} id='included' name='included' className="cluded-input" />
+        </div>
 
-        {!!data.results?.length && <div>Result: <ul>{data.results.map((result) =>
-          <li><b>{result}</b></li>
-        )}</ul></div>}
+        <div>
+          <label className="cluded-label" htmlFor='excluded'>Prevented digits: </label>
+          <input onChange={handleChange} id='excluded' name='excluded' className="cluded-input" />
+        </div>
+
+        <div>
+          <span className="cluded-label">Result:</span>
+          <div className="results-container">
+            {!!data.results?.length
+              ? data.results.map((result) => <span key={result}>{result}</span>)
+              : <span>No results</span>
+            }
+          </div>
+        </div>
       </div>
 
 
